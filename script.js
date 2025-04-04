7532a5ab3ae55530eabe1d22033e8bec45a886a5
suzouw <lucas_s_wisniewski@estudante.sesisenai.org.br>
2025-04-04 10:02:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+/*console.log('hello world');
+let n1 = parseFloat (prompt('escreva n1:'))
+let n2 = parseFloat (prompt('escreva n2'))
+let n3 = parseFloat (prompt('escreva n3'))
+
+function calcular (num1, num2, num3) {
+const soma = num1 + num2 + num3;
+console.log (soma)
+
+}
+
+
+calcular(n1,n2,n3);
+*/
+/*
+console.log('Contagem regresiva');
+let contagem = 10; 
+
+const contagemRegressiva = setInterval (() =>{
+    if(contagem >0){
+        console.log(contagem);
+        contagem--;
+    }else{
+        clearInterval(contagemRegressiva);
+        console.log('Lançamento');
+    }
+
+}, 1000);
+*/
+
+console.log ('Balanço mensal')
+let Jg = parseFloat (prompt('Escreva os gastos de janeiro'))
+let Jl = parseFloat (prompt('Escreva os ganhos de janeiro'))
+let Fg = parseFloat (prompt('Escreva os gastos de fevereiro'))
+let Fl = parseFloat (prompt('Escreva os ganhos de fevereiro'))
+let Mg = parseFloat (prompt('Escreva os gastos de Março'))
+let Ml = parseFloat (prompt('Escreva os ganhos de Março'))
+let Ag = parseFloat (prompt('Escreva os gastos de abril'))
+let AL = parseFloat (prompt('Escreva os ganhos de abril'))
+let Mg = parseFloat (prompt('Escreva os gastos de Maio'))
+let Ml = parseFloat (prompt('Escreva os ganhos de Maio'))
+let Julhog = parseFloat (prompt('Escreva os gastos de Julho'))
+let Julhog = parseFloat (prompt('Escreva os gastos de Julho'))
+
+
+
+function calcular (Jg, Jl, Fg, Fl,Mg, Ml, Ag, AL, Mg, Ml, Ag, AL, Mg, Ml, Julhog, Julho)
